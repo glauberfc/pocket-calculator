863879196a97a48df598a80633493cdbfc4a24a1
Glauber Castro <gfc714@gmail.com>
2022-06-16 10:03:15 -0300

    Set up project

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
