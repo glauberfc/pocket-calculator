e40c4a4dd4abb11f0c398813a477c7cfa5598cf8
Glauber Castro <gfc714@gmail.com>
2022-06-20 12:55:09 -0300

    Add Equal Experts colors

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,9 +1,16 @@
+import clsx from 'clsx'
 import useCalculator from 'hooks/useCalculator'
 import { Button, BUTTONS } from 'utils/constants'
 
 export default function Calculator() {
-  const { firstOperand, secondOperand, setOperand, setOperator, clear } =
-    useCalculator()
+  const {
+    firstOperand,
+    secondOperand,
+    operator,
+    setOperand,
+    setOperator,
+    clear,
+  } = useCalculator()
 
   function handleClick(item: Button) {
     if (item.type === 'operator') return setOperator(item.value)
@@ -14,25 +21,34 @@ export default function Calculator() {
   return (
     <div>
       <div
-        className="mb-4 p-4 text-right rounded-md border border-gray-400 bg-white"
+        className="mb-4 p-4 text-right text-3xl tracking-wider	font-semibold rounded-md border-2 border-secondary-600 text-white bg-transparent"
         aria-label="display"
       >
         {secondOperand === null ? firstOperand : secondOperand}
       </div>
-      <ul role="list" className="grid grid-cols-4 gap-1">
+      <ul role="list" className="grid grid-cols-4 gap-2">
         <li className="col-span-4">
           <button
-            className="w-full py-4 px-8 text-2xl text-center rounded-md border border-gray-400 bg-gray-200"
+            className="w-full py-3 px-8 text-xl text-center rounded-md bg-secondary-600 hover:bg-primary-600 text-white"
             onClick={clear}
           >
             Clear
           </button>
         </li>
         {BUTTONS.map((item) => {
+          const isOperand = item.type === 'operand'
+          const isOperatorSelected = item.value === operator
+
           return (
             <li key={item.key}>
               <button
-                className="w-full py-4 px-8 text-2xl text-center rounded-md border border-gray-400 bg-gray-200"
+                className={clsx(
+                  'w-full py-4 px-8 text-2xl text-center rounded-md',
+                  isOperand
+                    ? 'text-white bg-primary-400 hover:bg-primary-600'
+                    : 'hover:text-white bg-primary-100 hover:bg-primary-400',
+                  isOperatorSelected && 'ring-inset ring-4 ring-primary-500',
+                )}
                 onClick={() => handleClick(item)}
               >
                 {item.displayText}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#dff9ff',
+          400: '#1795d4',
+          500: '#1482b8',
+          600: '#196b98',
+        },
+        secondary: {
+          400: '#dddddd',
+          600: '#666666',
+          800: '#333333',
+          900: '#373D45',
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
